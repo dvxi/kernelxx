091cb80106e5d6dba61a063b8cfa14798b092f1e
pkorytowski <pkorytowski@hotmail.com>
2022-11-30 16:17:53 +0100

    add job info to main

--- src/components/SecondInfoSection.js
+++ src/components/SecondInfoSection.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Row, Col, Card, Image } from 'react-bootstrap';
 import { IoSchoolOutline, IoLaptopOutline, IoConstructOutline } from 'react-icons/io5';
+import { NavLink as Link } from "react-router-dom";
 
 const SecondInfoSection = () => {
 
@@ -95,6 +96,42 @@ return (
           </Card>
         </Col>
       </Row>
+
+        <Row className="text-center my-5">
+            <Col xs={12} lg={6} className="mx-auto">
+                <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" style={{
+                    position: 'absolute',
+                    marginTop: -100,
+                    left: '20%',
+                    height: '50vh',
+                    width: '50vw',
+                    zIndex: -1
+                }}>
+                    <path fill="#08BDBA" d="M29.3,-49.2C39.7,-44.7,51.1,-40.4,60.6,-32.2C70.1,-24.1,77.7,-12,78.5,0.5C79.3,13,73.4,26,63.7,33.8C54.1,41.7,40.6,44.4,29.4,54.4C18.1,64.4,9.1,81.6,-2.3,85.7C-13.7,89.7,-27.5,80.6,-41.6,72.3C-55.8,63.9,-70.3,56.5,-78.8,44.5C-87.3,32.6,-89.7,16.3,-88.3,0.8C-86.8,-14.6,-81.4,-29.3,-73,-41.2C-64.5,-53.2,-53,-62.5,-40.3,-65.7C-27.6,-68.9,-13.8,-66,-2.2,-62.2C9.4,-58.4,18.8,-53.7,29.3,-49.2Z" transform="translate(100 100)" />
+                </svg>
+                <div data-aos="fade-right">
+                    <h2>Praca szuka człowieka!</h2>
+                    <p className="lead">
+                        Sprawdź oferty pracy przygotowane przez naszych sponsorów
+                    </p>
+                </div>
+            </Col>
+        </Row>
+        <Row className="text-center my-5 justify-content-center">
+            <Col xs={8} lg={6} className="mb-5">
+                <Card className="p-3 text-black mb-5" style={{ borderRadius: 15 }} data-aos="zoom-in">
+                    <Link to="jobboard" className="nav-link">
+                    <Card.Body>
+                        <h3 style={{ fontWeight: 700 }}>
+                            Kliknij by przejść <br/> na nasz JobBoard!
+                        </h3>
+
+                    </Card.Body>
+                    </Link>
+                </Card>
+            </Col>
+        </Row>
+
       </Container>
       <Container flex className="text-center py-2">
         <p>Projekt i wykonanie: <a rel="nofollow" href="https://www.linkedin.com/in/szymongwozdz/" style={{
